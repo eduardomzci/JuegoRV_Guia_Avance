2f4665263b0d9c7660bf8876ea04535a4dcc79a5
Eduardo Mercado Zurita <eduardomzci@hotmail.com>
2022-02-21 18:09:01 -0400

    lfs archivos

---
+++ Assets/Script/VuforiaEscaneo/TpVuforia.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TpVuforia : MonoBehaviour
+{
+    public void cambiarEscena_MapaPrincipal_1()
+    {
+        SceneManager.LoadScene("MapaPrincipal", LoadSceneMode.Single);
+    }
+
+    public void cambiarEscena_MapaPrincipal_2()
+    {
+        SceneManager.LoadScene("MapaPrincipal", LoadSceneMode.Single);
+    }
+}
